ff61bcacb1cc0e87cd74d048e3a10efae223360e
沈凯明 <shenkaiming@bianfeng.com>
2021-10-26 18:02:50 +0800

    fix typo

--- {{cookiecutter.project_slug}}/{{cookiecutter.project_slug}}/urls.py
+++ {{cookiecutter.project_slug}}/{{cookiecutter.project_slug}}/urls.py
@@ -45,9 +45,9 @@ urlpatterns += [
     path('api/auth-token/', obtain_auth_token),
     path('api/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
     path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
-    path(r'swagger(?P<format>\.json|\.yaml)$', schema_view.without_ui(cache_timeout=0), name='schema-json'),
-    path(r'swagger/$', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
-    path(r'redoc/$', schema_view.with_ui('redoc', cache_timeout=0), name='schema-redoc'),
+    path(r'swagger(?P<format>\.json|\.yaml)', schema_view.without_ui(cache_timeout=0), name='schema-json'),
+    path(r'swagger/', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
+    path(r'redoc/', schema_view.with_ui('redoc', cache_timeout=0), name='schema-redoc'),
 
 ]
 {%- endif %}
