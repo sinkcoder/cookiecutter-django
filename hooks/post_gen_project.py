5920a7d3b1dd28fc0cf90b0a75f19c2f8434dd69
沈凯明 <shenkaiming@bianfeng.com>
2021-10-28 16:45:08 +0800

    docker-compose add mysql service

--- hooks/post_gen_project.py
+++ hooks/post_gen_project.py
@@ -204,25 +204,25 @@ def generate_random_user():
     return generate_random_string(length=32, using_ascii_letters=True)
 
 
-def generate_postgres_user(debug=False):
+def generate_mysql_user(debug=False):
     return DEBUG_VALUE if debug else generate_random_user()
 
 
-def set_postgres_user(file_path, value):
-    postgres_user = set_flag(file_path, "!!!SET POSTGRES_USER!!!", value=value)
-    return postgres_user
+def set_mysql_user(file_path, value):
+    mysql_user = set_flag(file_path, "!!!SET MYSQL_USER!!!", value=value)
+    return mysql_user
 
 
-def set_postgres_password(file_path, value=None):
-    postgres_password = set_flag(
+def set_mysql_password(file_path, value=None):
+    mysql_password = set_flag(
         file_path,
-        "!!!SET POSTGRES_PASSWORD!!!",
+        "!!!SET MYSQL_PASSWORD!!!",
         value=value,
         length=64,
         using_digits=True,
         using_ascii_letters=True,
     )
-    return postgres_password
+    return mysql_password
 
 
 def set_celery_flower_user(file_path, value):
@@ -250,22 +250,22 @@ def append_to_gitignore_file(s):
         gitignore_file.write(os.linesep)
 
 
-def set_flags_in_envs(postgres_user, celery_flower_user, debug=False):
+def set_flags_in_envs(mysql_user, celery_flower_user, debug=False):
     local_django_envs_path = os.path.join(".envs", ".local", ".django")
     production_django_envs_path = os.path.join(".envs", ".production", ".django")
-    local_postgres_envs_path = os.path.join(".envs", ".local", ".postgres")
-    production_postgres_envs_path = os.path.join(".envs", ".production", ".postgres")
+    local_mysql_envs_path = os.path.join(".envs", ".local", ".mysql")
+    production_mysql_envs_path = os.path.join(".envs", ".production", ".mysql")
 
     set_django_secret_key(production_django_envs_path)
     set_django_admin_url(production_django_envs_path)
 
-    set_postgres_user(local_postgres_envs_path, value=postgres_user)
-    set_postgres_password(
-        local_postgres_envs_path, value=DEBUG_VALUE if debug else None
+    set_mysql_user(local_mysql_envs_path, value=mysql_user)
+    set_mysql_password(
+        local_mysql_envs_path, value=DEBUG_VALUE if debug else None
     )
-    set_postgres_user(production_postgres_envs_path, value=postgres_user)
-    set_postgres_password(
-        production_postgres_envs_path, value=DEBUG_VALUE if debug else None
+    set_mysql_user(production_mysql_envs_path, value=mysql_user)
+    set_mysql_password(
+        production_mysql_envs_path, value=DEBUG_VALUE if debug else None
     )
 
     set_celery_flower_user(local_django_envs_path, value=celery_flower_user)
