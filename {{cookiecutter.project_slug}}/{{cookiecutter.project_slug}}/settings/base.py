c3c6db50c66a1ea10e68ed0a082fbe2cb9d53b77
沈凯明 <shenkaiming@bianfeng.com>
2021-11-15 19:32:46 +0800

    turn on cors

--- {{cookiecutter.project_slug}}/{{cookiecutter.project_slug}}/settings/base.py
+++ {{cookiecutter.project_slug}}/{{cookiecutter.project_slug}}/settings/base.py
@@ -330,7 +330,8 @@ REST_FRAMEWORK = {
 
 # django-cors-headers - https://github.com/adamchainz/django-cors-headers#setup
 CORS_URLS_REGEX = r'^/api/.*$'
-
+CORS_ALLOW_CREDENTIALS = True
+CORS_ORIGIN_ALLOW_ALL = True
 {%- endif %}
 # Your stuff...
 # ------------------------------------------------------------------------------
